ea67fdc3e313af8c9996e3166e340d629b759dc3
Nishant Sharma <nishants1994@gmail.com>
2016-06-07 01:37:35 -0500

    Added interface and implementaion for kinematic bug
    Kinematic bug currently uses rabbit carrot planning based path prunning approach to smooth out curves

--- include/bug_flood/bug_flood.h
+++ include/bug_flood/bug_flood.h
@@ -5,15 +5,16 @@
 #ifndef BUG_FLOOD_BUG_FLOOD_H
 #define BUG_FLOOD_BUG_FLOOD_H
 
-#define PUBLISH_PATH
-
 #include <ros/ros.h>
 #include <vector>
 #include <visualization_msgs/Marker.h>
 #include <bug_flood/bug.h>
+#include <bug_flood/type_decls.h>
 typedef visualization_msgs::Marker Marker;
 
 using namespace std;
+
+#define PUBLISH_PATH
 
 #ifdef PUBLISH_PATH
 void initMarkers(Marker &bugs, Marker &pathTree);
@@ -21,5 +22,15 @@ void publish_bugs(vector<Bug> &bugList, ros::Publisher &publisher, Marker &bugs,
 void publish_bug(Bug &bug, ros::Publisher &publisher, Marker &pathTree);
 #endif
 
+#define KINEMATIC_BUG
+
+#ifdef KINEMATIC_BUG
+
+#define RABBIT_STEP_SIZE 0.5
+
+bool updateCarrotWhenMovingOnLine(Line line, Point &rabbit, Point &carrot);
+void updateRabbit(Point &rabbit, Point carrot);
+void pruneRabbitCarrot(vector<Point> &inPath, double &cost);
+#endif
 
 #endif //BUG_FLOOD_BUG_FLOOD_H
--- src/bug_flood_extra.cpp
+++ src/bug_flood_extra.cpp
@@ -80,3 +80,89 @@ void publish_bug(Bug &bug, ros::Publisher &publisher, Marker &pathTree)
 }
 
 #endif
+
+#ifdef KINEMATIC_BUG
+
+bool updateCarrot(Line line, Point &rabbit, Point &carrot)
+{
+	bool isIncrement = false;
+
+	/** get robot projection on the current line that is being followed **/
+	Point rabbitIntersection;
+	getPerpendicularLineIntersection(line, rabbit, rabbitIntersection);
+
+	/** place carrot a fixed distance ahead of robot on that line **/
+	double carrotHeadingAngle = getDirectionAngleRadian(line.end, line.start);
+
+	Point carrotNewPosition;
+	carrotNewPosition.x = rabbitIntersection.x + ((5) * cos(carrotHeadingAngle));
+	carrotNewPosition.y = rabbitIntersection.y + ((5) * sin(carrotHeadingAngle));
+
+	/** check if carrot is still on the line segment or not **/
+	double carrotToLineStart, lineLength;
+	carrotToLineStart  = getEuclideanDistance(carrotNewPosition, line.start);
+	lineLength = getEuclideanDistance(line.start, line.end);
+
+	/** if carrot is crossing the line segment
+			then carrot location is waypoint value and carrot state is reached waypoint
+		else
+			carrot location is the new carrot location
+	**/
+	if (carrotToLineStart > lineLength)
+	{
+		carrot = line.end;
+		isIncrement = true;
+	}
+	else
+	{
+		carrot = carrotNewPosition;
+	}
+	return isIncrement;
+}
+
+void updateRabbit(Point &rabbit, Point carrot)
+{
+	/** update  **/
+	double headingAngle = getDirectionAngleRadian(carrot, rabbit);
+	rabbit.x = rabbit.x + ((RABBIT_STEP_SIZE) * cos(headingAngle));
+	rabbit.y = rabbit.y + ((RABBIT_STEP_SIZE) * sin(headingAngle));
+}
+
+void pruneRabbitCarrot(vector<Point> &inPath, double &cost)
+{
+	vector< geometry_msgs::Point > bugPath;
+	/**
+		Final path was reversed. Reversing it back to make it from source to goal;
+	*/
+	int i;
+	vector< Point > path;
+
+	/** Rabbit's start point is source and then pushing start point to the new path */
+	Point rabbit = bugPath[0];
+	path.push_back(rabbit);
+
+	Point carrot;
+	bool changeLine = false;
+	i = 1;
+
+	/** loop to traverse from source to dest and generate path according to carrot-following algorithm*/
+	while(i < inPath.size())
+	{
+		Line line;
+		line.start = inPath[i-1];
+		line.end = inPath[i];
+		changeLine = updateCarrot(line, rabbit, carrot);
+		updateRabbit(rabbit, carrot);
+		path.push_back(rabbit);
+		if(changeLine)
+		{
+			i++;
+		}
+	}
+	path.push_back(bugPath[bugPath.size()-1]);
+	inPath = path;
+}
+
+
+#endif
+
